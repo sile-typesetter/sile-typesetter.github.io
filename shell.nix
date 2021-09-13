85b4256bc9ee68b10c4cdb462a1b00e02be7d62e
Caleb Maclennan <caleb@alerque.com>
2021-09-13 13:10:24 +0300

    Use Nix's github-pages packages for Jekyll
    Co-authored-by: ilkecan <ilkecan@protonmail.com>

--- shell.nix
+++ shell.nix
@@ -1,16 +1,5 @@
 with import <nixpkgs> {};
-
-let
-  env = bundlerEnv {
-    name = "sile-typesetter.org";
-    inherit ruby;
-    gemfile = ./Gemfile;
-    lockfile = ./Gemfile.lock;
-    gemset = ./gemset.nix;
-  };
-
-in stdenvNoCC.mkDerivation {
-  name = "sile-typesetter.org";
-  buildInputs = [ env ruby ];
-  nativeBuildInputs = [ bash sile ];
+mkShell {
+  inputsFrom = [ rubyPackages.github-pages ];
+  nativeBuildInputs = [ bash gnumake sile ];
 }

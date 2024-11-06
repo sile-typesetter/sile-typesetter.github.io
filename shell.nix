007fb78a6084f921e4f1776ae1189ff60f4c5d73
Caleb Maclennan <caleb@alerque.com>
2024-11-06 17:54:02 +0300

    Restyle Nix code with nixfmt

--- shell.nix
+++ shell.nix
@@ -1,5 +1,22 @@
-{ nixpkgs ? import (fetchTarball https://nixos.org/channels/nixos-unstable/nixexprs.tar.xz) {} }:
+{
+  nixpkgs ? import (fetchTarball "https://nixos.org/channels/nixos-unstable/nixexprs.tar.xz") { },
+}:
 with nixpkgs;
 mkShell {
-  nativeBuildInputs = [ bash cacert curl ghostscript git gnumake imagemagick libarchive nix poppler_utils rsync sile yq zola ];
+  nativeBuildInputs = [
+    bash
+    cacert
+    curl
+    ghostscript
+    git
+    gnumake
+    imagemagick
+    libarchive
+    nix
+    poppler_utils
+    rsync
+    sile
+    yq
+    zola
+  ];
 }
